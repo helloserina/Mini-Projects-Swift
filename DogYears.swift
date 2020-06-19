9df3f2572415e7dfc99c7463e2f17cdf4cd38293
helloserina <57045574+helloserina@users.noreply.github.com>
2020-06-19 14:04:43 -0700

    Create DogYears.swift
    Swift program in DogYears.swift to calculate the age, in human years, of any dog older than 2.

---
+++ DogYears.swift
@@ -0,0 +1,12 @@
+var dogAge = 2//clyde age
+
+var earlyYears: Int
+var laterYears: Int
+var humanYears: Int
+
+earlyYears = 21
+laterYears = dogAge - 2
+laterYears *= 4
+
+humanYears = earlyYears + laterYears
+print("My name is Clyde! Ruff ruff, I am \(humanYears) years old in human years.")
